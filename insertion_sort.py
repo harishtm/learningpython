6bd58dc576b103bebcd10ea3c37b150d95dcce98
Harish tm <harishtm77@gmail.com>
2017-07-02 12:46:43 +0530

    Fixed indentaion issues

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,18 @@
 import sys
 
 def insertion_sort(array):
-	for index in range(1, len(array)):
-		while 0 < index and array[index] < array[index -1]:
-			array[index], array[index-1] = array[index-1], array[index]
-			index -= 1
-	return array
+    for index in range(1, len(array)):
+        while 0 < index and array[index] < array[index -1]:
+            array[index], array[index-1] = array[index-1], array[index]
+            index -= 1
+    return array
 
 
 if __name__ == "__main__":
-	if sys.version_info.major < 3:
-		input_function = raw_input
-	else:
-		input_function = input
-	user_input = input_function("Enter numbers seperated by comma : ")
-	unsortedlist = [int(i) for i in user_input.split(',')]
-	print insertion_sort(unsortedlist)
+    if sys.version_info.major < 3:
+        input_function = raw_input
+    else:
+        input_function = input
+    user_input = input_function("Enter numbers seperated by comma : ")
+    unsortedlist = [int(i) for i in user_input.split(',')]
+    print insertion_sort(unsortedlist)
--- shell_sort.py
+++ shell_sort.py
@@ -2,25 +2,25 @@ import sys
 
 
 def shell_sort(array):
-	n = len(array)
-	gap = n/2
-	while gap > 0:
-		for i in range(gap, n):
-			temp = array[i]
-			j = i
-			while j >= gap and array[j-gap] > temp:
-				array[j] = array[j-gap]
-				j -= gap
-			array[j] =  temp
-		gap /= 2
-	return array
+    n = len(array)
+    gap = n/2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = array[i]
+            j = i
+            while j >= gap and array[j-gap] > temp:
+                array[j] = array[j-gap]
+                j -= gap
+            array[j] =  temp
+        gap /= 2
+    return array
 
 
 if __name__ == '__main__':
-	if sys.version_info.major < 3:
-		input_function = raw_input
-	else:
-		input_function = input
-	user_input = input_function("Enter numbers seperated by comma : ")
-	unsortedlist = [int(item) for item in user_input.split(',')]
-	print shell_sort(unsortedlist)
+    if sys.version_info.major < 3:
+        input_function = raw_input
+    else:
+        input_function = input
+    user_input = input_function("Enter numbers seperated by comma : ")
+    unsortedlist = [int(item) for item in user_input.split(',')]
+    print shell_sort(unsortedlist)
