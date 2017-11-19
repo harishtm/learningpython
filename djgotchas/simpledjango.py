535721850eeb7f19d094adab4e8f37ed0f253c98
Harish tm <harishtm77@gmail.com>
2017-11-19 13:45:01 +0530

    A simple django application like flask

---
+++ djgotchas/simpledjango.py
@@ -0,0 +1,31 @@
+"""
+	To run python simpledjango.py runserver
+"""
+
+import sys
+from django.conf import settings
+from django.conf.urls import url
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
+
+
+settings.configure(
+	DEBUG=True,
+	SECRET_KEY='zwqrtttt123bbbrf',
+	ROOT_URLCONF=sys.modules[__name__]
+)
+
+
+def index(request):
+	return HttpResponse('<h1>A simple Django</h1>')
+
+
+urlpatterns = [
+	url(r'^$', index)
+]
+
+
+if __name__ == '__main__':
+	execute_from_command_line(sys.argv)
+
+
